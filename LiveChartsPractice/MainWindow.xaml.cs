939b7260ab69d77ca519106b708c7e4d2303f4ed
Jinge <jinge_windows@hotmail.com>
2020-04-28 08:12:02 +1000

    添加了简单的“柱状图”，“曲线图”，“行状图”。 但是目前有个bug，就是第一次启动程序的时候，tab_0的内容不能自动加载出来。

--- LiveChartsPractice/MainWindow.xaml.cs
+++ LiveChartsPractice/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
 
 namespace LiveChartsPractice
 {
@@ -20,9 +25,74 @@ namespace LiveChartsPractice
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+
+
+        private void TabControl_HomePage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabControl tabControl = (TabControl)e.Source;
+            int tabIndex = tabControl.SelectedIndex;
+            switch (tabIndex)
+            {
+                case 0:
+
+                    SeriesCollection series_0 = new SeriesCollection
+                    {
+                        new LineSeries
+                        {
+                            Title = "曲线图",
+                            Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                        },
+                    };
+
+                    UserControl_Chart chart_0 = new UserControl_Chart();
+                    chart_0.Description = "单线曲线图，值类型为double，只设置了Title和Values属性，鼠标悬停显示Title";
+                    chart_0.Series = series_0;
+                    TabItem_Linechart.Content = chart_0;
+
+                    break;
+
+                case 1:
+                    SeriesCollection series_1 = new SeriesCollection
+                    {
+                        new ColumnSeries
+                        {
+                            Title = "柱状图",
+                            Values = new ChartValues<double> { 4, 6, 5, 2, 4 }
+                        }
+                    };
+                    UserControl_Chart chart_1 = new UserControl_Chart();
+                    chart_1.Description = "单柱状图，值类型为double，只设置了Title和Values属性，鼠标悬停显示Title";
+                    chart_1.Series = series_1;
+                    TabItem_Columnchart.Content = chart_1;
+
+                    break;
+                case 2:
+                    SeriesCollection series_2 = new SeriesCollection
+                    {
+                        new RowSeries
+                        {
+                            Title = "行状图",
+                            Values = new ChartValues<double> { 4, 6, 5, 2, 4 }
+                        }
+                    };
+                    UserControl_Chart chart_2 = new UserControl_Chart();
+                    chart_2.Description = "单行状图，值类型为double，只设置了Title和Values属性，鼠标悬停显示Title";
+                    chart_2.Series = series_2;
+                    TabItem_Rowchart.Content = chart_2;
+                    break;
+            }
+        }
+
+
+
+
+
     }
 }
---
+++ LiveChartsPractice/UserControl_Chart.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace LiveChartsPractice
+{
+    /// <summary>
+    /// UserControl_Chart.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserControl_Chart : UserControl
+    {
+        //窗口下面的描述文字
+        public string Description { get; set; }
+        //图表
+        public SeriesCollection Series { get; set; }
+
+
+
+
+        public UserControl_Chart()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
